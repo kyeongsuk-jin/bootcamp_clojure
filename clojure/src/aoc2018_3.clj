70dc77995dc7dc7bce28a66668877304abcd0eef
kyeongsuk-jin <118232876+kyeongsuk-jin@users.noreply.github.com>
2022-11-29 10:00:24 +0900

    DAY1-5 (#2)

--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -1,4 +1,6 @@
-(ns aoc2018_3)
+(ns aoc2018_3
+  (:require [clojure.set :as set]
+            [aoc2018 :as aoc]))
 
 
 ;; 파트 1
@@ -8,7 +10,8 @@
 ;; #2 @ 3,1: 4x4
 ;; #3 @ 5,5: 2x2
 
-;; # 뒤에 오는 숫자는 ID, @ 뒤에 오는 숫자 쌍 (a, b)는 시작 좌표, : 뒤에 오는 (c x d)는 격자를 나타냄.
+;; # 뒤에 오는 숫자는 ID, @ 뒤에 오는 숫자 쌍 (a, b)는 시작 좌표,
+;; : 뒤에 오는 (c x d)는 격자를 나타냄.
 ;; 입력의 정보대로 격자 공간을 채우면 아래와 같이 됨.
 
 ;;      ........
@@ -23,10 +26,68 @@
 ;; 여기서 XX는 ID 1, 2, 3의 영역이 두번 이상 겹치는 지역.
 ;; 겹치는 지역의 갯수를 출력하시오. (위의 예시에서는 4)
 
+; rematched 사용
+(defn parse-input [str]
+  "입력 받은 문자열을 Map 으로 변경한다.
 
+  ex) (parse-grid-input \"#1 @ 1,3: 4x4\")
+  ;=> {:id 1, :start-r 1, :start-c 3, :inc-r 4, :inc-c 4}
+  "
+  (let [match-format #"^#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)"
+        matched (rest (re-matches match-format str))]
+    (when (some? matched)
+      (zipmap [:id :start-r :start-c :inc-r :inc-c]
+              (->> matched (mapv parse-long)))
+      )))
 
+(defn init-grid [parsed-input]
+  "값을 넣을 격자를 만든다.
+  parsed-input -> {:id :start-r :start-c :inc-r :inc-c}
+
+  key -> vector
+  value -> set
+
+  ex)
+  ;=> {[a b] #{id}}
+  "
+  (let [{:keys [id start-r start-c inc-r inc-c]} parsed-input]
+    (for [row (range start-r (+ start-r inc-r))
+          col (range start-c (+ start-c inc-c))]
+      {[row col] #{id}}
+      )))
+
+(def merged-grid
+  " init-grid 를 merge 한다.
+  ex)
+  {[4 3] #{1 2},
+   [2 3] #{1},
+   [2 5] #{1},
+   [3 3] #{1 2}}"
+  (->> (aoc/read-file "aoc2018-3-1.input")
+       (map parse-input)
+       (mapcat init-grid)
+       (apply merge-with set/union)))
+
+(defn filter-conflicted? [[k v]]
+  "{[a b] #{id}}
+  id set 이 1보다 큰 지
+  "
+  (let [c (count v)] (> c 1)))
+
+; {[a b] #{id}} -> set 사이즈가 1보다 크면 count
+(->> merged-grid
+     (filter filter-conflicted?)
+     (count))
 
 ;; 파트 2
 ;; 입력대로 모든 격자를 채우고 나면, 정확히 한 ID에 해당하는 영역이 다른 어떤 영역과도 겹치지 않음
 ;; 위의 예시에서는 ID 3 이 ID 1, 2와 겹치지 않음. 3을 출력.
 ;; 겹치지 않는 영역을 가진 ID를 출력하시오. (문제에서 답이 하나만 나옴을 보장함)
+
+(set/difference (->> merged-grid
+                     (vals)
+                     (apply set/union))
+                (->> merged-grid
+                     (filter filter-conflicted?)
+                     (vals)
+                     (apply set/union)))
--- clojure/src/aoc2018_5.clj
+++ clojure/src/aoc2018_5.clj
@@ -1,4 +1,5 @@
-(ns aoc2018_5)
+(ns aoc2018_5
+  (:require [clojure.string :as string]))
 ;; 파트 1
 ;; 입력: dabAcCaCBAcCcaDA
 
@@ -10,7 +11,73 @@
 
 ;; 주어진 input 에서 최종으로 남는 문자열을 리턴하시오.
 
+(def input "dabAcCaCBAcCcaDA")
+
+(defn remove-char
+  "result map 에서 마지막 문자를 지운다.
+
+  {
+  :last-ch - result 의 마지막 char
+  :result - 반응하는 결과 string
+  }
+  "
+  [m]
+  (let[{:keys [result]} m
+       removed-result (subs result 0 (dec (count result)))]
+    (assoc m :last-ch (last removed-result) :result removed-result)))
+
+(defn merge-result
+  "map 에 char 를 합치면서 반응을 적용한다.
+
+   {
+   :last-ch - result 의 마지막 char
+   :result - 반응하는 결과 string
+   }
+  "
+  [m char]
+  (let [{:keys [last-ch result]} m
+        required-diff (- (int \a) (int \A))
+        diff (abs (- (int last-ch) (int char)))]
+    (if (= diff required-diff)
+        (remove-char m)
+        (assoc m :last-ch char :result (str result char)))))
+
+(defn merge-upper-lower
+  "문자열에 chars seq 를 받아서 반응 결과를 리턴한다.
+  "
+  [seq-list]
+  (-> (reduce
+        (fn [m char]
+          (let [{:keys [last-ch]} m]
+            (if (nil? last-ch)
+              (assoc m :last-ch char :result (str char))
+              (merge-result m char))))
+        {:result "" :last-ch nil} seq-list) :result)
+  )
+
+(merge-upper-lower input)
+
 ;; 파트 2
 ;; 주어진 문자열에서 한 유닛 (대문자와 소문자)을 전부 없앤 후 반응시켰을 때, 가장 짧은 문자열의 길이를 리턴하시오.
 ;; 예를 들어 dabAcCaCBAcCcaDA 에서 a/A를 없애고 모두 반응시키면 dbCBcD가 되고 길이는 6인데 비해,
 ;; 같은 문자열에서 c/C를 없애고 모두 반응시키면 daDA가 남고 길이가 4이므로 4가 가장 짧은 길이가 됨.
+
+(defn merge-removed-alphabet-upper-lower
+  "입력 받은 char 를 지우고 merge 한다."
+  [input char]
+  (let [regex (str "(?i)" char)
+        pattern (re-pattern regex)
+        removed-str (string/replace input pattern "")]
+    (merge-upper-lower removed-str)
+  ))
+
+(defn reduced-chars
+  "입력받은 str 에 대문자 set 을 만들고 각 char 별로 지운 뒤 merge 한다."
+  [str]
+  (let [chars (->> input string/upper-case distinct vec)]
+    (for [char chars] (merge-removed-alphabet-upper-lower str char))))
+
+(->> (reduced-chars input)
+     (map count)
+     (apply min)
+     )
